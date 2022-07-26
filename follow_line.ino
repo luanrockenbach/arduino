776af0cacb7934f780824d712ca398648fe7cc7d
Luan Rockenbach <71732714+luanrockenbach@users.noreply.github.com>
2022-07-26 10:01:12 -0300

    creation of functions to control the motors

--- follow_line.ino
+++ follow_line.ino
@@ -4,6 +4,15 @@ int leftSensor = A5;
 
 int led = 13;
 
+#define left_wheelGo 3
+#define left_wheelBack 5
+#define right_wheelGo 6
+#define right_wheelBack 9
+
+int max_speed = 255;
+int middle_speed = 150;
+int min_speed = 50;
+
 
 void serialPrint(char sensor, int value)
 {
@@ -33,6 +42,32 @@ int middleSensorFunction()
   return(value);  
 }
 
+void stopMotor()
+{
+  analogWrite(left_wheelGo, 0);
+  analogWrite(left_wheelBack, 0);
+  analogWrite(right_wheelGo, 0);
+  analogWrite(right_wheelBack, 0);
+}
+
+void dickVigarista()
+{
+  analogWrite(right_wheelGo, middle_speed);
+  analogWrite(left_wheelGo, middle_speed);
+}
+
+void rightCurve()
+{
+  analogWrite(right_wheelGo, min_speed);
+  analogWrite(left_wheelGo, middle_speed);
+}
+
+void leftCurve()
+{
+  analogWrite(right_wheelGo, middle_speed);
+  analogWrite(left_wheelGo, min_speed);
+}
+
 
 void setup() 
 {
@@ -40,6 +75,13 @@ void setup()
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   digitalWrite(led,HIGH);
+
+  pinMode(left_wheelGo, OUTPUT);
+  pinMode(left_wheelBack, OUTPUT);
+  pinMode(right_wheelGo, OUTPUT);
+  pinMode(right_wheelBack, OUTPUT);
+
+  stopMotor();
 }
 
 void loop() {
@@ -55,4 +97,7 @@ void loop() {
   Serial.println("Left Sensor:");
   leftSensorFunction();
   delay(100);
+
+  dickVigarista();
+  
 }
