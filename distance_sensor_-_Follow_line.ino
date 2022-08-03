60982b33865cf8b572f50b64946a79c1150b739b
Luan Rockenbach <71732714+luanrockenbach@users.noreply.github.com>
2022-08-03 15:45:47 -0300

    NOT F* WORK

--- distance_sensor_-_Follow_line.ino
+++ distance_sensor_-_Follow_line.ino
@@ -12,7 +12,7 @@ int led = 13;
 
 // Motor speed
 int max_speed = 255;
-int middle_speed = 150;
+int middle_speed = 120;
 int min_speed = 50;
 
 // Ultrasonic sensor
@@ -20,6 +20,7 @@ int min_speed = 50;
 #define trigger 8
 
 int sensorControll = 28;
+int buzzer = 12;
 
 
 void serialPrint(char sensor, int value)
@@ -27,28 +28,60 @@ void serialPrint(char sensor, int value)
   Serial.println("Sensor "+(String)sensor+" value="+(String)value);
 }
 
+// ----------------------------------------- BUZZER ---------------------------------------
 
-// LEITURA DOS SENSORES
+int buzzy(unsigned int timing) // gera buzina por 'ms'
+{
+  digitalWrite(buzzer, HIGH);
+  delay(timing);
+  digitalWrite(buzzer, LOW);
+}
+
+void angry_buzzy(){
+  buzzy(2000); // buzina por 2s
+  delay(200);
+  buzzy(200); // buzina por 0,5s
+  delay(200);
+  buzzy(200); // buzina por 0,5s
+  delay(100);
+  buzzy(500); // buzina por 0,5s
+}
+
+// ------------------------------------ OBSTÁCULO -----------------------------------------
+
+void barrier()
+{
+  if(superSonic() <= 10)
+  {
+    stopMotor();
+    angry_buzzy();
+  }
+}
+
+
+// ------------------------------ LEITURA DOS SENSORES -------------------------------------
 int rightSensorFunction()
 {
   int value = analogRead(rightSensor);
-  serialPrint('R', value);
+  //serialPrint('R', value);
   return(value);  
 }
 
 int leftSensorFunction()
 {
   int value = analogRead(leftSensor);
-  serialPrint('L', value);
+  //serialPrint('L', value);
   return(value);  
 }
 
 int middleSensorFunction()
 {
   int value = analogRead(middleSensor);
-  serialPrint('M', value);
+  //serialPrint('M', value);
   return(value);  
 }
+
+// ------------------------------- CONTROLE DE MOTORES -----------------------------------------
 
 void stopMotor()
 {
@@ -63,9 +96,29 @@ void dickVigarista(const int right, const int left)
   analogWrite(right_wheelGo, right);
   analogWrite(right_wheelBack, 0);
   
-  analogWrite(left_wheelGo, left + 0.86);
+  analogWrite(left_wheelGo, left + 1);
   analogWrite(left_wheelBack, 0);
 }
+
+// ------------------------------- LEITURA SUPERSÔNICO ------------------------------------------
+
+int superSonic() // retorna distância estimada em cm
+{
+  long int lenght;
+  int distance; /* em cm */
+
+  digitalWrite(trigger, LOW);
+  delay(100);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+
+  lenght = pulseIn(echo, HIGH, 100000);
+  distance = lenght * 0.034 / 2;
+  return(distance);
+  }
+
+// ------------------------------------ CONTROLE ------------------------------------------------
 
 void setup() 
 {
@@ -79,38 +132,38 @@ void setup()
   pinMode(right_wheelGo, OUTPUT);
   pinMode(right_wheelBack, OUTPUT);
 
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer, LOW);
+
+
   stopMotor();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /*Serial.println("Right Sensor:");
-  rightSensorFunction();
-  delay(100);
-  Serial.println("Middle Sensor:");
-  middleSensorFunction();
-  delay(100);
-  Serial.println("Left Sensor:");
-  leftSensorFunction();
-  delay(100);*/
+  int d = superSonic();
+  Serial.println(d);
 
-  while(rightSensorFunction() >sensorControll && leftSensorFunction() >sensorControll)
-  {
-   dickVigarista(middle_speed, middle_speed);
-  }
+   while(rightSensorFunction() >sensorControll && leftSensorFunction() >sensorControll)
+   {
+    dickVigarista(middle_speed, middle_speed);
+   }
+    
+   while(rightSensorFunction() > sensorControll && leftSensorFunction() <= sensorControll)
+   {
+    dickVigarista(middle_speed, min_speed);
+   }
   
-  while(rightSensorFunction() > sensorControll && leftSensorFunction() <= sensorControll)
-  {
-   dickVigarista(middle_speed, min_speed);
-  }
-
-  while(rightSensorFunction() <= sensorControll && leftSensorFunction() > sensorControll)
-  {
-   dickVigarista(min_speed, middle_speed);
-  }
-
-  if(rightSensorFunction() <=sensorControll && leftSensorFunction() <=sensorControll)
-  {
+   while(rightSensorFunction() <= sensorControll && leftSensorFunction() > sensorControll)
+   {
+    dickVigarista(min_speed, middle_speed);
+   }
+  
+   if(rightSensorFunction() <=sensorControll && leftSensorFunction() <=sensorControll)
+   {
     stopMotor();
-  }
+   }
 }
